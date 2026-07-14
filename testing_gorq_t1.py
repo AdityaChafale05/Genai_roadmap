0cf15098464cbe209e3c2ee54d97d27dbee0a68a
AdityaChafale05 <adityachafale2006@gmail.com>
2026-07-14 19:42:22 +0530

    Made a Structur for working of Gemini APi - Just models need to be updated or change

--- testing_gorq_t1.py
+++ testing_gorq_t1.py
@@ -1,3 +1,5 @@
+"""
+Code for Groq API    ヾ(⌐■_■)ノ♪
 
 import httpx
 import os
@@ -29,4 +31,69 @@ if response.status_code == 200 :
 else:
     print("something went wrong : " ,response.status_code,response.text)
     
-        
+    
+"""
+
+"""
+ヾ(⌐■_■)ノ♪
+
+Below code is for Gemini API - ( change models as they might not work )  
+
+import httpx 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api = os.getenv("api_gemini")
+
+response = httpx.post (
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent",
+    
+    headers={"x-goog-api-key": api},
+    
+    json={
+        "contents": [
+            {"parts": [{"text": "Tell me a joke about college, relatable to students"}]}
+        ]
+    },
+    timeout=30.0
+)
+
+if response.status_code == 200 :
+    data = response.json()
+    
+    reply = data["candidates"][0]["content"]["parts"][0]["text"]
+    token_usage = data["usageMetadata"]["totalTokenCount"]
+    print(reply)
+    print("Token Usage:", token_usage)
+else:
+    print("Something went wrong:" ,response.status_code, response.text)    
+    
+
+"""
+
+
+"""
+TO check the available models in Gemini API
+
+import httpx
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("api_gemini")
+
+response = httpx.get(
+    "https://generativelanguage.googleapis.com/v1beta/models",
+    headers={"x-goog-api-key": api_key},
+    timeout=30.0
+)
+
+if response.status_code == 200:
+    data = response.json()
+    for model in data["models"]:
+        print(model["name"])
+else:
+    print("Something went wrong:", response.status_code, response.text)
+    
+"""
